0b017497dffe9009a3c07d670295a327ff7fb687
Leo <leonardovieras@gmail.com>
2025-03-24 20:40:17 -0300

    arreglé cosas y ahora muestra la página principal al menos

--- recetariobud/recetariobase/views.py
+++ recetariobud/recetariobase/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    return render(request, "recetariobase/home.html")
 
 def ver_recetas(request):
-    return render(request, "ver_recetas.html")
+    return render(request, "recetariobase/ver_recetas.html")
 
 def agregar_receta(request):
-    return render(request, "agregar_receta.html")
+    return render(request, "recetariobase/agregar_receta.html")
