61357e3a5801f73bf03220672004b6075c2c4592
Leo <leonardovieras@gmail.com>
2025-03-24 20:51:30 -0300

    intentando mostrar recetas en /recetas

--- recetariobud/recetariobase/views.py
+++ recetariobud/recetariobase/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-
+from .models import Receta, RecetaIngrediente
 # Create your views here.
 def home(request):
     return render(request, "recetariobase/home.html")
 
 def ver_recetas(request):
-    return render(request, "recetariobase/ver_recetas.html")
+    recetas = Receta.objects.all()
+    
+    rec_ingredientes = []
+
+    for receta in recetas:
+        ingredientes = RecetaIngrediente.objects.filter(receta=receta)
+        rec_ingredientes.append({"receta": receta, "ingredientes": ingredientes})
+    context = {"recetas":recetas, "rec_ingredientes":rec_ingredientes}
+    return render(request, "recetariobase/ver_recetas.html", context)
 
 def agregar_receta(request):
     return render(request, "recetariobase/agregar_receta.html")
