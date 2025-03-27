7bf0228320768859265a1957e1342e056bc148e6
Leo <leonardovieras@gmail.com>
2025-03-26 22:28:40 -0300

    creo que finalmente podré mostar las recetas, falta cargar alguna tho

--- recetariobud/recetariobase/views.py
+++ recetariobud/recetariobase/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from .models import Receta, RecetaIngrediente
+from collections import defaultdict
 # Create your views here.
 def home(request):
     return render(request, "recetariobase/home.html")
 
 def ver_recetas(request):
-    recetas = Receta.objects.all()
+    recetas_ingredientes = RecetaIngrediente.objects.select_related("receta", "ingrediente").all()
     
-    rec_ingredientes = []
+    # Agrupar los ingredientes por receta
+    recetas_dict = defaultdict(list)
+    for rec_ing in recetas_ingredientes:
+        recetas_dict[rec_ing.receta].append(rec_ing)
 
-    for receta in recetas:
-        ingredientes = RecetaIngrediente.objects.filter(receta=receta)
-        rec_ingredientes.append({"receta": receta, "ingredientes": ingredientes})
-    context = {"recetas":recetas, "rec_ingredientes":rec_ingredientes}
+    context = {"recetas_dict":recetas_dict}
     return render(request, "recetariobase/ver_recetas.html", context)
+
+
 
 def agregar_receta(request):
     return render(request, "recetariobase/agregar_receta.html")
